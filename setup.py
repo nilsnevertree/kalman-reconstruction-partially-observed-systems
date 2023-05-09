ff2e577910c7d59d20626ea7e7d32fa05ac6c6fd
nilsnevertree <nils.niebaum@gmx.de>
2023-05-09 16:17:00 +0200

    initial commit
    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data-driven Reconstruction of Partially Observed Dynamical Systems using Kalman algorithms and an itterative procedure.',
+    author='IMT-Atlantique-MEE',
+    license='',
+)
