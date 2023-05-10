e2280ab5ee9fc1f296c8e919434b6fe38bdafb7f
nilsnevertree <nils.niebaum@gmx.de>
2023-05-10 15:04:30 +0200

    gaussian weights added

--- kalman_reconstruction/statistics.py
+++ kalman_reconstruction/statistics.py
@@ -20,3 +20,29 @@ def my_mean(x):
     """
 
     return np.mean(x)
+
+
+def gaussian_weights(x, y, alpha):
+    """Creates a Gaussian
+    This functions only purpose is to check the CI workflows of the repository.
+
+    Parameters
+    ----------
+    x: np.ndarray
+        Array for which the mean shall be calculated.
+
+    Returns
+    -------
+    np.ndarray
+        Mean of input array x.
+        ndim=1
+
+    """
+    # apply the Gaussian kernel
+    weights = np.exp((-((x - y) ** 2)) / (2 * alpha**2))
+
+    # normalize the weights
+    weights = weights / np.sum(weights)
+
+    # return the weights
+    return weights
