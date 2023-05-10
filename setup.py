7a6dfe8b0b19b1b8bcb6617a8a8b31e727f1184a
nilsnevertree <nils.niebaum@gmx.de>
2023-05-10 10:34:38 +0200

    First code linting
    First linting of the whole code performed using pre-commit.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
+
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Data-driven Reconstruction of Partially Observed Dynamical Systems using Kalman algorithms and an itterative procedure.',
-    author='IMT-Atlantique-MEE',
-    license='',
+    version="0.1.0",
+    description="Data-driven Reconstruction of Partially Observed Dynamical Systems using Kalman algorithms and an itterative procedure.",
+    author="IMT-Atlantique-MEE",
+    license="",
 )
