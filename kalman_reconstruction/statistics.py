06ef8fd5abcb2622d3d09a66acef4c5113c9a679
nilsnevertree <nils.niebaum@gmx.de>
2023-05-10 16:37:08 +0200

    visualized 2D weight problem for linear regression

--- kalman_reconstruction/statistics.py
+++ kalman_reconstruction/statistics.py
@@ -22,22 +22,36 @@ def my_mean(x):
     return np.mean(x)
 
 
-def gaussian_weights(x, y, alpha):
-    """Creates a Gaussian
-    This functions only purpose is to check the CI workflows of the repository.
+def gaussian_weights(x, y, axis=0, alpha=0.2):
+    """Creates a Gaussian weights for a 2D-array x centered at positions given by y.
 
     Parameters
     ----------
     x: np.ndarray
-        Array for which the mean shall be calculated.
+        Array of dimension (n, m).
+        ndim should be 2
+    y: np.ndarray
+        Array of dimension (n, ).
+        ndim should be 2 but 1 is also accepted.
+        if ndim is 1
+    alpha: float
+        Alpha of the gaus distribution.
+        Default to 0.2
 
     Returns
     -------
     np.ndarray
-        Mean of input array x.
-        ndim=1
+        gaussian weights
+        dimension as x
 
     """
+    try:
+        assert np.array_equal(np.ndim(x), np.ndim(y))
+    except Exception:
+        if axis == 0:
+            y = y[:, np.newaxis]
+        elif axis == 1:
+            y = y[np.newaxis, :]
     # apply the Gaussian kernel
     weights = np.exp((-((x - y) ** 2)) / (2 * alpha**2))
 
